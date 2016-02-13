add60986327dd8b580e70736c2ce87807dedce8b
MacMacZhl <macmaczhl@gmail.com>
2016-02-13 22:43:51 +0300

    NavigationActivity Added

---
+++ mainp5/src/com/example/view/FragmentOne.java
@@ -0,0 +1,113 @@
+package com.example.view;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import com.example.controller.Lesson;
+import com.example.controller.LessonAdapter;
+import com.example.p5.R;
+import com.example.xml.XMLSerialize;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.AdapterView.OnItemSelectedListener;
+
+public class FragmentOne extends Fragment {
+	Spinner spinner;
+	public static int selected;
+	
+	View bufView;
+	
+	public FragmentOne()
+	{
+		
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+	
+		View rootView = inflater.inflate(R.layout.activity_main, container,
+				false);
+		
+		return rootView;
+	}
+	
+	@Override
+	public void onViewCreated(View viewMain, Bundle savedInstanceState) {
+		//////////////////
+		bufView = viewMain;
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+				android.R.layout.simple_spinner_item, getResources()
+						.getStringArray(R.array.weekday));
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner = (Spinner) viewMain.findViewById(R.id.spinner1);
+		spinner.setAdapter(adapter);
+
+		Calendar calendar = Calendar.getInstance();
+		spinner.setSelection(calendar.get(Calendar.DAY_OF_WEEK) - 1);
+
+		spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view,
+					int position, long id) {
+				selected = spinner.getSelectedItemPosition() + 1;
+				if (selected == 8)
+					selected = 7;
+				ShowList();
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+			}
+		});
+		///////////////////
+	}
+	
+	public void ShowList() {
+
+		ListView mylist = (ListView) bufView.findViewById(R.id.gridView);
+		try {
+			List<Lesson> allLessons = new ArrayList<Lesson>();
+			List<Lesson> currentList = new ArrayList<Lesson>();
+			allLessons = XMLSerialize.read().list;
+
+			for (Lesson obj : allLessons) {
+				if (obj.getDay() == selected)
+					currentList.add(obj);
+			}
+			LessonAdapter adapter = new LessonAdapter(getActivity(), currentList);
+			adapter.SetActivity(this);
+			if (allLessons.isEmpty())
+				mylist.setAdapter(null);
+			else {
+				mylist.setAdapter(adapter);
+			}
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			mylist.setAdapter(null);
+		}
+	}
+	
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.buttonAdd:
+			AddDialog dlg1 = new AddDialog();
+			dlg1.show(getFragmentManager(), "228");
+			dlg1.SetActivity(this);
+			break;
+		}
+	}
+}
